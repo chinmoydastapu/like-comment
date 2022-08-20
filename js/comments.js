d63527a2267abefba088aecbe0d4fbfbb913c62a
chinmoydastapu <chinmoydastapu2@gmail.com>
2022-08-20 13:14:43 +0600

    bug fixed and now showing comments section added

--- js/comments.js
+++ js/comments.js
@@ -1,3 +1,4 @@
+//Disabling post button while comment box field is empty
 document.getElementById('input-content').addEventListener('keyup', function () {
     const commentBoxField = document.getElementById('input-content');
     if (commentBoxField.value !== '') {
@@ -7,11 +8,19 @@ document.getElementById('input-content').addEventListener('keyup', function () {
     }
 });
 
+//Counting comments
 let countComments = 0;
 document.getElementById('post-comment-btn').addEventListener('click', function () {
     countComments++;
     document.getElementById('comments-count').innerText = countComments + ' comments';
-    document.getElementById('input-content').value = '';
-    document.getElementById('input-content').value.length = 0;
-    console.log(document.getElementById('input-content').value.length);
+
+    //Appending the comments above write comment box field
+    const commentBoxField = document.getElementById('input-content');
+    const p = document.createElement('p');
+    p.innerText = 'Person ' + countComments + ' Commented:- ' + commentBoxField.value;
+    document.getElementById('show-comments').appendChild(p);
+
+    //Again empty the comment box and set the post button disabled
+    commentBoxField.value = '';
+    document.getElementById('post-comment-btn').setAttribute('disabled', true);
 });
